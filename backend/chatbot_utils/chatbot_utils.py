33a2eaf9b6a0a166767a9b078f8b287f962778b5
SAint7579 <vishwa20@outlook.com>
2023-11-18 17:22:31 +0100

    Added the chatbot utils functions and increased the token limit

--- backend/chatbot_utils/chatbot_utils.py
+++ backend/chatbot_utils/chatbot_utils.py
@@ -6,7 +6,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain, RetrievalQAWithSourcesChain
 from langchain.memory import ConversationBufferMemory
 
-from context_model import *
+import context_model as cm
 import os
 
 import pickle
@@ -20,6 +20,14 @@ os.environ["OPENAI_API_KEY"] = key['openai_api_key']
 
 
 def chat_with_bot(question, log_file_hash, chat_id, memory=False):
+
+    ## Creating the embedding object
+    embedding = OpenAIEmbeddings(
+                openai_organization=key['openai_organization'],
+                openai_api_key = key['openai_api_key'],
+                model="text-embedding-ada-002",
+                max_retries=10,
+                )
 
     if not memory:
         memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True, input_key='question', output_key='answer')
@@ -38,6 +46,33 @@ def chat_with_bot(question, log_file_hash, chat_id, memory=False):
     docs = docs_small + docs_large
     conf = conf_small + conf_large
 
+    ## Creating the context vectorstore
     df = pd.DataFrame({'docs':docs, 'conf':conf})
     df.sort_values(by='conf', ascending=False, inplace=True)
     df.reset_index(drop=True, inplace=True)
+    vectorstore_relevant = Chroma.from_documents(list(df['docs'].values), embedding=embedding)
+    
+    ## Creating the chatbot
+    llm = ChatOpenAI(
+        openai_organization="org-kfsNXpcw90CoawqSyD7Mw4CD",
+        model="gpt-4",
+        max_tokens=1000,
+        )
+
+    ## Creating the retriever
+    retriver = vectorstore_relevant.as_retriever(search_kwargs={"k": 10,"score_threshold": .5})
+    chat = RetrievalQAWithSourcesChain.from_llm(llm, 
+                                                retriever=retriver, 
+                                                memory=memory, 
+                                                return_source_documents=False,
+                                                max_tokens_limit=1000, 
+                                                reduce_k_below_max_tokens=True)
+
+    result = chat({"question": question})
+
+    ## Saving the memory
+    with open(f'./memory/{chat_id}_memory.pkl', 'wb') as f:
+        pickle.dump(memory, f)
+
+    return {'answer': result['answer'], 'Source':[i.page_content for i in df['docs'].values[:10]], 'Confidence': [i for i in df['conf'].values[:10]]}
+
